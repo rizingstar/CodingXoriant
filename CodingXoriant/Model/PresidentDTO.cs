ed8f3f4b393299ebd7a9d0dbad4e4e10d0d3d2a1
nomankhan <rizingstar@yahoo.com>
2020-05-12 03:01:32 -0500

    * LogEvents.cs: * PresidentDTO.cs:
    * CodingXoriant.deps.json: Added Logging, added REST Custom Return
      LogEvents Status Codes

---
+++ CodingXoriant/Model/PresidentDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CodingXoriant.Model
+{
+    public class PresidentDTO
+    {
+        [JsonProperty("president")]
+        public string PresidentName { get; set; }
+        [JsonProperty("number")]
+        public int Number { get; set; }
+        [JsonProperty("birth_year")]
+        public int Birth_year { get; set; }
+        [JsonProperty("death_year")]
+        public int? Death_year { get; set; }
+        [JsonProperty("took_office")]
+        public DateTime TookOffice { get; set; }
+        [JsonProperty("left_office")]
+        public DateTime LeftOffice { get; set; }
+        [JsonProperty("party")]
+        public int? Party { get; set; }
+    }
+}
